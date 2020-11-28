9f8827a846b90298c4a8f39d806e9a3d17b0c41a
leonardo-rubert <leonardo.rubert@edu.pucrs.br>
2020-11-28 20:43:11 -0300

    diagrama finalizado

--- LocadoraV1semDB/LocadoraBackEnd/src/main/java/com/grupo12/Entidades/Repositorio/Locacoes.java
+++
@@ -1,9 +0,0 @@
-package com.grupo12.Entidades.Repositorio;
-
-import com.grupo12.Entidades.Dominio.Locacao.Locacao;
-import com.grupo12.Entidades.Dominio.Veiculo.Carro;
-
-//carros nao disponiveis
-public interface Locacoes extends Repositorio<Carro,Long>{
-    
-}
