49d784068cad6e8ada5bed5cbbf91df520c3647f
unknown <jean.uchaki@acad.pucrs.br>
2020-11-30 22:16:56 -0300

    fazendo testes

--- LocadoraV1semDB/LocadoraBackEnd/src/test/java/com/grupo12/ApplicationTests.java
+++ LocadoraV1semDB/LocadoraBackEnd/src/test/java/com/grupo12/ApplicationTests.java
@@ -1,13 +1,11 @@
 package com.grupo12;
 
-//import com.grupo12.Entidades.Descontos.FactoryDesconto;
+import java.text.ParseException;
+
+import com.grupo12.CasosDeUso.ControleDeLocacao;
 import com.grupo12.Entidades.Descontos.FactoryDesconto;
 import com.grupo12.Entidades.Dominio.Locacao.Locacao;
-//import com.grupo12.Entidades.Dominio.Seguro.RegraSeguro;
-//import com.grupo12.Entidades.Dominio.Seguro.RegraSeguro;
 import com.grupo12.Entidades.Servicos.Servicos;
-//import com.grupo12.Entidades.Dominio.Locacao.*;
-//import com.grupo12.Interface.Persistencia.Frota.FrotaDB;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,50 +14,82 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
-	private Servicos ser = null;
+	private Servicos servico;
+	private ControleDeLocacao ctrl;
+	private FactoryDesconto desconto;
+	private Locacao l1;
+	private Locacao l2;
+	private DataLocal i1;
+	private DataLocal i2;
 
 	@BeforeEach
-	void setUp(){ser = new Servicos(null, null);
-	}
-	@Test
-	void calcDiaTeste() {
-		DataLocal c = new DataLocal();
-		DataLocal b = new DataLocal();
-		c.setMes(11);
-		b.setMes(12);
-		Locacao a = new Locacao("ABC129", "ACME", "M4", false, false, false, c, b, false);
-		double e = ser.calculaDiaria(a);
-		Assertions.assertEquals(1500, e);
+	void setUp() {
+		i1 = new DataLocal();
+		i2 = new DataLocal();
+		l1 = new Locacao("ABC129", "ACME", "M4", false, false, false, i1, i2, false);
+		l1 = new Locacao("ABC129", "ACME", "M4", false, false, false, i1, i2, false);
 	}
 
 	@Test
-	 void calcDiasTeste(){
-		DataLocal c = new DataLocal();
-		DataLocal b = new DataLocal();
-		c.setMes(11);
-		b.setMes(12);
-		Locacao a = new Locacao("ABC129", "ACME", "M4", false, false, false, c, b, false);
-		double e = ser.calculaDias(a);
-		Assertions.assertEquals(30, e);
+	void calcDiariaTeste() {
+		servico = new Servicos(null, null);
+		l1.getFimLocacao().setDia(10);
+		l1.getInicioLocacao().setDia(1);
+		Assertions.assertEquals(9,servico.calculaDias(l1));
 	}
-	@Test
-	void calcSeguroTeste(){
-		DataLocal c = new DataLocal();
-		DataLocal b = new DataLocal();
-		c.setMes(11);
-		b.setMes(12);
-		Locacao a = new Locacao("ABC129", "ACME", "M4", false, false, false, c, b, false);
-		double e = ser.calculaDesconto(a);
-		Assertions.assertEquals(0.5, e);
+
+	void calcDiariariaTeste() {
+		servico = new Servicos(null, null);
+		l1.getFimLocacao().setDia(10);
+		l1.getInicioLocacao().setDia(1);
+		Assertions.assertEquals(450,servico.calculaDiaria(l1));
 	}
+
 	@Test
-	void calcTotalTeste(){
-		DataLocal c = new DataLocal();
-		DataLocal b = new DataLocal();
-		c.setMes(11);
-		b.setMes(12);
-		Locacao a = new Locacao("ABC129", "ACME", "M4", false, false, false, c, b, false);
-		double e = ser.calculaTotal(a);
-		Assertions.assertEquals(30, e);
+	void calcDescontoNormalTeste() {
+		desconto = new FactoryDesconto();
+		i1.setMes(11);
+		desconto.getRegraVerificacao(l1);
+		Assertions.assertEquals(0.2, desconto.getRegraVerificacao(l1).disconto());
+	}
+
+	@Test
+	void calcDescontoEspecialTeste() {
+		desconto = new FactoryDesconto();
+		i1.setMes(12);
+		desconto.getRegraVerificacao(l1);
+		Assertions.assertEquals(0.5, desconto.getRegraVerificacao(l1).disconto());
+	}
+
+	@Test
+	void validaDataMenorQueAtualTesteFalso() {
+		ctrl = new ControleDeLocacao(null);
+		i1.setAno(i1.getDia()-1);
+		try {
+			Assertions.assertEquals(false, ctrl.validaDataAtual(i1));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	void validaDataMenorQueAtualTesteTrue() throws ParseException {
+		ctrl = new ControleDeLocacao(null);
+		i1.setDia(i1.getDia()+1);
+		Assertions.assertEquals(true, ctrl.validaDataAtual(i1));
+	}
+
+	@Test
+	void validaDataFinalMenorInicialTrue() throws ParseException {
+		ctrl = new ControleDeLocacao(null);
+		i2.setDia(i1.getDia()+1);
+		Assertions.assertEquals(true, ctrl.validaDataPeriodo(i1, i2));
+	}
+
+	@Test
+	void validaDataFinalMenorInicialFalse() throws ParseException {
+		ctrl = new ControleDeLocacao(null);
+		i2.setDia(i1.getDia()-1);
+		Assertions.assertEquals(false, ctrl.validaDataPeriodo(i1, i2));
 	}
 }
